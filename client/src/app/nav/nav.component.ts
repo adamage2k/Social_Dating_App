a5fbdd53a628433240c3560c8e8aecd7daf506be
Sassanek <adriano2250@gmail.com>
2022-04-05 19:07:39 +0200

    Creating routing

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,32 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
-import {AccountService } from '../services/account.service';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
 
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  constructor(public accountService: AccountService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    this.accountService.logIn(this.model).subscribe(
+      (response) => {
+        this.router.navigateByUrl('/members'), console.log(response);
+      },
+      (error) => {
+        console.log(error);
+        this.toastr.error(error.error);
+      }
+    );
   }
 
-  login(){
-    this.accountService.logIn(this.model).subscribe(response => {
-      console.log(response);
-    }, error => {
-      console.log(error);
-    });
-  }
-
-  logout(){
+  logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
-
 }
