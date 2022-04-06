f22d5f1b695f9a30b28e52b7db4a3a87fd4a4447
adrianMoskal <adrian.moskal@icloud.com>
2022-04-06 09:02:03 +0200

    Add and configure AutoMapper

---
+++ backend/SocialDatingApp.Application/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialDatingApp.Application.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            //Basic
+            //CreateMap<fromClass, toClass>();
+
+            //Advanced
+            //CreateMap<fromClass, toClass>()
+            //    .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Id));
+        }
+    }
+}
