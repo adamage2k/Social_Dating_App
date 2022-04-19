87699252c575b5bf5b4bbf895c94d2c1b19e447b
adrianMoskal <adrian.moskal@icloud.com>
2022-04-19 21:49:49 +0200

    Add migration with connections table

---
+++ backend/SocialDatingApp.Infrastructure/Configuration/ConnectionConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialDatingApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialDatingApp.Infrastructure.Configuration
+{
+    public class ConnectionConfiguration : IEntityTypeConfiguration<Connection>
+    {
+        public void Configure(EntityTypeBuilder<Connection> builder)
+        {
+            builder.ToTable("Connection");
+
+            builder
+                .HasOne(c => c.User1)
+                .WithMany(u => u.Sent)
+                .HasForeignKey(c => c.UserId1);
+
+            builder
+                .HasOne(c => c.User2)
+                .WithMany(u => u.Received)
+                .HasForeignKey(c => c.UserId2);
+        }
+    }
+}
---
+++ backend/SocialDatingApp.Infrastructure/Migrations/20220419194718_AddConnectionTable.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SocialDatingApp.Infrastructure.Migrations
+{
+    public partial class AddConnectionTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Connection",
+                columns: table => new
+                {
+                    ConnectionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId1 = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    UserId2 = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Connection", x => x.ConnectionId);
+                    table.ForeignKey(
+                        name: "FK_Connection_User_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Connection_User_UserId2",
+                        column: x => x.UserId2,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connection_UserId1",
+                table: "Connection",
+                column: "UserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connection_UserId2",
+                table: "Connection",
+                column: "UserId2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Connection");
+        }
+    }
+}
