a71ac52bb0314a847e7afe369d76ea3cd7ef05c8
adrianMoskal <adrian.moskal@icloud.com>
2022-06-01 22:27:01 +0200

    Integration changes

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -18,22 +18,12 @@ export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm;
   member: Member;
   user: User;
-  @HostListener('window:beforeunload', ['$event']) unloadNotification(
-    $event: any
-  ) {
-    if (this.editForm.dirty) {
-      $event.returnValue = true;
-    }
-  }
 
   constructor(
-    private accountService: AccountService,
     private memberService: MembersService,
     private toastr: ToastrService
   ) {
-    this.accountService.currentUser$
-      .pipe(take(1))
-      .subscribe((user) => (this.user = user));
+    this.loadMember(); 
   }
 
   ngOnInit(): void {
@@ -43,7 +33,7 @@ export class MemberEditComponent implements OnInit {
   loadMember() {
     this.memberService
       .getSelfProfile()
-      .subscribe((member) => (this.member = member));
+      .subscribe((member) => this.member = member);
   }
 
   updateMember() {
