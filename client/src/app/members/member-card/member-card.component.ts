2ffd687f8643040e034beb968e73ea863a631e9c
Sassanek <adriano2250@gmail.com>
2022-06-01 21:25:14 +0200

    Adding like feature

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,25 +1,30 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { MembersService } from 'src/app/members.service';
 import { Member } from 'src/app/models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrls: ['./member-card.component.css']
-  
+  styleUrls: ['./member-card.component.css'],
 })
 export class MemberCardComponent implements OnInit {
   @Input() member: Member;
 
-  constructor(private router: Router) { }
+  constructor(
+    private memberService: MembersService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
-    console.log(this.member)
+    console.log(this.member);
   }
 
-  onButtonClick(username: string) {
-    console.log(username)
-    this.router.navigate(['/members', username]);
+  addMatch(member: Member) {
+    this.memberService.addMatch(member.username).subscribe(
+      () => this.toastr.success('Match added + member.knowsAs'),
+      (error) => this.toastr.error('Something went wrong')
+    );
   }
-
 }
